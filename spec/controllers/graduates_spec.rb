c01416a87d43c38005d833b0c4bffd998a106b77
Jordan Kamin <Jordan@Jordans-MacBook-Air.local>
2014-06-13 17:53:35 -0700

    functional passing controller test

--- spec/controllers/graduates_spec.rb
+++ spec/controllers/graduates_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe GraduatesController do
   it "#search" do
-     get :search  {location: "San Francisco", year: "2014"}
+     get :search,  {location: "San Francisco", year: "2014"}
      expect(response.status).to eq(200)
   end
 end
