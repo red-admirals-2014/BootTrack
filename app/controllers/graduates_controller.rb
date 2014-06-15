34fd9b56cb2b0bd85cb1fa3be675a35d6269a676
Alejandro E. Losada <alosada@gmail.com>
2014-06-14 23:37:54 -0700

    its a commit

--- app/controllers/graduates_controller.rb
+++ app/controllers/graduates_controller.rb
@@ -15,8 +15,8 @@ class GraduatesController < ApplicationController
     p campus
     graduates = Graduate.joins(:cohort).select("graduates.*, cohorts.start_date as start_date, cohorts.location as location").order('start_date DESC').first(30) if campus=='default' && year=='default'
     graduates = Cohort.where(location: campus).joins(:graduates).select('cohorts.*,graduates.name as name').order('start_date DESC').first(30) if campus!='default' && year=='default'
-    #graduat'es = Cohort.joins(:graduates).order('start_date DESC').where('extract(year  from start_date) = ?', year).limit(3) if campus=='default' && year!='default'
-    #graduates = Cohort.joins(:graduates).order('start_date DESC').where(location: campus).where('extract(year  from start_date) = ?', year).limit(3) if campus!='default' && year!='default'
+    graduates = Cohort.joins(:graduates).order('start_date DESC').where('extract(year  from start_date) = ?', year).limit(30) if campus=='default' && year!='default'
+    graduates = Cohort.joins(:graduates).order('start_date DESC').where(location: campus).where('extract(year  from start_date) = ?', year).limit(30) if campus!='default' && year!='default'
     return graduates
   end
 
