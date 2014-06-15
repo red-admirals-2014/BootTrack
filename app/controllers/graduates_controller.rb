1f15ecacbbec266f9409b8250db1b5c71bb3621c
Alejandro E. Losada <alosada@gmail.com>
2014-06-14 19:47:38 -0700

    Display cards is working for no filter

--- app/controllers/graduates_controller.rb
+++ app/controllers/graduates_controller.rb
@@ -5,17 +5,17 @@ class GraduatesController < ApplicationController
   def search
     campus = params[:campus]
     year = params[:year]
-    graduates = { graduates: 'Ale' }.to_json #get_graduates(campus, year)
+    graduates = get_graduates(campus, year)
     render json: { graduates: graduates }.to_json, :status => :ok
   end
 
   private
 
   def get_graduates(campus, year)
-    graduates = Graduate.all if campus=='default' && year=='default'
-    graduates = Graduate.where(campus: campus) if campus!='default' && year=='default'
-    graduates = Graduate.where(year: year) if campus=='default' && year!='default'
-    graduates = Graduate.where(year: year, campus: campus) if campus=='default' && year!='default'
+    graduates = Graduate.joins(:cohort).select("graduates.*, cohorts.start_date as s_date, cohorts.location as campus").first(30) if campus=='default' && year=='default'
+    #graduates = Cohort.joins(:graduates).order('start_date DESC').where(location: campus).limit(3) if campus!='default' && year=='default'
+    #graduates = Cohort.joins(:graduates).order('start_date DESC').where('extract(year  from start_date) = ?', year).limit(3) if campus=='default' && year!='default'
+    #graduates = Cohort.joins(:graduates).order('start_date DESC').where(location: campus).where('extract(year  from start_date) = ?', year).limit(3) if campus!='default' && year!='default'
     return graduates
   end
 
--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -2,9 +2,4 @@ class Cohort < ActiveRecord::Base
   has_many :graduates
   attr_accessible :id, :location, :start_date
 
-  def self.get_cohort
-    @cohort = DBC::Cohort.all.to_json
-    JSON.parse(@cohort)
-  end
-
 end
