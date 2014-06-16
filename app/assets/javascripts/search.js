7a57287128e8ae9f80231be2cc5cd160793fba58
Alejandro & Melissa Alvarez <pair+alosada.alvarezmelissa87@devbootcamp.com>
2014-06-15 19:23:22 -0700

    Working on styling the initial search bar

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
-  search = new Search()
-  search.start();
+  view = new View()
+  bootTrack = new BootTrack(view)
+  bootTrack.start();
 })
 
-function Search() {}
+function BootTrack(view) { //Main controller.
+  this.view = view
+}
 
-Search.prototype = {
+BootTrack.prototype = {
   start: function(){
     $('form').on('submit', this.getGraduates)
-    $('[data-comp="topbar"]').on('click', '[data-comp="search-again"]', this.searchAgain)
+    $('[data-comp="topbar"]').on('click', '[data-comp="search-again"]', this.view.searchAgain)
     $('[data-comp="topbar"]').on('click', '[data-comp="view-map"]',test)
   },
 
@@ -19,24 +22,35 @@ Search.prototype = {
       type: 'get',
       data: $('form').serialize()
     })
-    ajaxCall.done(showGrads)
-    ajaxCall.fail(test)
-  },
+    ajaxCall.done(view.showGrads);
+    ajaxCall.fail(test);
+  }
+
+}
+
+function View(){};
+
+View.prototype = {
   searchAgain: function(e){
-    e.preventDefault()
+    e.preventDefault();
     $(".card-container").html("")
     $('.hidable').show()
-  }
-}
+    $('[data-comp="topbar"]').hide();
+  },
 
- function test(res){
-  debugger
-  console.log("test")
- }
-
-function showGrads(res){
-  $('.hidable').hide()
+  showGrads: function(res){
+  $('[data-comp="topbar"]').show();
+  $('.hidable').hide();
   var grad_template = "{{#graduates}}<div class='card'><img src='{{picture}}'><h3>{{name}}</h3>DBC {{campus}}<br>{{start_date}}<br>{{employer}}<br>{{location}}<br><br><button>Contact Me!</button></div>{{/graduates}}";
   var html = Mustache.to_html(grad_template, res);
-  $(".card-container").html(html)
-}
+  $(".card-container").html(html);
+  }
+};
+
+
+ function test(res){
+  console.log("You've hit the test function. Congratulations.")
+  debugger
+ }
+
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,9 @@ module ApplicationHelper
     begin
       new_info = client.profile(:url => grad.linked_in , :fields => ['headline', 'location:(name)'])
       grad.update_attributes(employer: new_info.headline, location: new_info.location.name)
+      p '==========='
+      p '=  Succes ='
+      p '==========='
     rescue StandardError => e
       p "Error: #{e}"
       grad.update_attributes(employer: nil, location: nil)
@@ -15,7 +18,7 @@ module ApplicationHelper
   def update_all_linked_in
     Graduate.all.each do |grad|
       unless grad.linked_in == nil
-        grad.get_linked_in
+        ApplicationHelper.get_linked_in(grad)
         sleep 2
       end
     end
