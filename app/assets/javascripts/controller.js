11791874ebb74a8151d67b7471b8f2821c7fe00b
Melissa Alvarez <alvarezmelissa87@gmail.com>
2014-06-16 17:51:07 -0700

    everything working after merge with master

--- app/assets/javascripts/controller.js
+++ app/assets/javascripts/controller.js
@@ -34,13 +34,13 @@ BootTrack.prototype = {
   },
 
  sendEmail: function(e){
-  debugger
   e.preventDefault();
+  debugger
     var request = $.ajax({
         type: 'POST',
         url: '#',
         data: $('form.contact-form').serialize(),
-        success: function (data) {
+        success: function (data) {     ////data is  this"user_name=derek&user_email=&message=&id=962"
             alert('ok');
         }
     });
@@ -64,7 +64,6 @@ View.prototype = {
   },
 
   showGrads: function(res){
-    debugger
     $('[data-comp="topbar"]').show();
     $('.hidable').hide();
     var grad_template = "{{#graduates}}<div class='card'><img src='{{picture}}'><h3>{{name}}</h3>DBC {{campus}}<br>{{start_date}}<br>{{employer}}<br>{{location}}<br><br><button id={{id}} class='contact'>Contact Me!</button></div>{{/graduates}}";
