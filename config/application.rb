7b10290af92ee76c5d31acfbaf9e749280a287a5
Melissa Alvarez <alvarezmelissa87@gmail.com>
2014-06-17 20:03:40 -0700

    finally getting heroku working

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ require "mustache"
 require "gravatar-ultimate"
 require "geocoder"
 
-config.assets.initialize_on_precompile = false
 
 
 if defined?(Bundler)
@@ -27,6 +26,7 @@ module BootTrack
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.initialize_on_precompile = false
 
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/lib)
