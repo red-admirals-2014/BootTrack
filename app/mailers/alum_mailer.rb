10e1b1620838171eec784c268121bbcd42905861
Melissa Alvarez <alvarezmelissa87@gmail.com>
2014-06-18 14:10:26 -0700

    Email working again

--- app/mailers/alum_mailer.rb
+++ app/mailers/alum_mailer.rb
@@ -2,10 +2,12 @@ class AlumMailer < ActionMailer::Base
   default from: "BootsTrack@gmail.com"
 
   def contact_attempt(grad, name, email, message)
+    p "*" * 50
+    p grad
     @name = name
     @message = message
     @grad = grad
     @email = email
-    mail to: @grad.email, subject: "Someone wants to contact you about DBC!"
+    mail to: grad.email, subject: "Someone wants to contact you about DBC!"
   end
 end
