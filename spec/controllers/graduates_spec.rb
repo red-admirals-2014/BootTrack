50f625a0c8a3c7099441aad84412036688acdfa1
Jordan Kamin <Jordan@Jordans-MacBook-Air.local>
2014-06-18 21:14:48 -0700

    pre-merge commit

--- spec/controllers/graduates_spec.rb
+++ spec/controllers/graduates_spec.rb
@@ -1,16 +1,25 @@
   require "spec_helper"
 
-describe GraduatesController do
-  context "#search" do
-    it "gives success on good params" do
+  describe GraduatesController do
+    context "#search" do
+      it "gives success on good params" do
        get :search,  {campus: "San Francisco", year: "2014"}
        expect(response.status).to eq(200)
-    end
-    it "gives error on bad params" do
+     end
+     it "gives error on no params" do
       pending "There's no error condition built into the code"
 
-       get :search,  {campus: "Chicago", year: "2012"}
-       expect(response.status).to eq(404)
+      get :search,  {}
+      expect(response.status).to eq(500)
     end
   end
+
+  context "#locaion" do
+
+  end
+
+  context "#email" do
+
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,6 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   #config.include CapybaraHelpers
-
   config.use_transactional_fixtures = false
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
